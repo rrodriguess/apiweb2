c2b1e013fda439e505f2885e86930727e7848064
Renato <renato.mello@paulistano.org.br>
2016-08-25 11:06:14 -0300

    Modificado estrutura

---
+++ src/main/java/com/br/renato/Apiweb2Application.java
@@ -0,0 +1,12 @@
+package com.br.renato;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Apiweb2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Apiweb2Application.class, args);
+	}
+}
