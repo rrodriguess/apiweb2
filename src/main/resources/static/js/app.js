3cd62752a9a8486c6a8a01c5d2779f336a6da292
Renato <rrodriguez@outlook.com.br>
2016-09-13 14:31:04 -0300

    Concluido aula 5.1

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -2,58 +2,6 @@
 var appCliente = angular.module("appCliente", []);
 
 //Criação de controllers
-appCliente.controller("indexController", function($scope, $http) {
-
-    $scope.clientes = [];
-    $scope.cliente={}; //Binding com o form
-
-    $scope.carregarClientes = function() {
-        $http({method:'GET', url:'http://localhost:8081/clientes'})
-            .then(function(response){
-                $scope.clientes = response.data;
-
-            }, function(response){
-                console.log(response.data);
-                console.log(response.status);
-        });
-    };
-
-    $scope.salvarCliente = function() {
-        $http({method:'POST', url:'http://localhost:8081/clientes', data:$scope.cliente})
-            .then(function(response){
-                $scope.clientes.push(response.data);
-
-            }, function(response){
-                console.log(response.data);
-                console.log(response.status);
-        });
-    };
-
-    $scope.excluirCliente = function(cliente) {
-        $http({method:'DELETE', url:'http://localhost:8081/clientes/'+cliente.id})
-            .then(function(response){
-            //Buscar posição  e remover cliente do Array.
-            pos = $scope.clientes.indexOf(cliente);
-            $scope.clientes.splice(pos, 1);
-
-            }, function(response){
-                console.log(response.data);
-                console.log(response.status);
-        });
-    };
-
-    $scope.alterarCliente = function(cli) {
-        $scope.cliente = angular.copy(cli);
-    };
-
-    $scope.cancelarAlteracaoCliente = function() {
-        $scope.cliente={};
-    };
-
-    $scope.carregarClientes();
-
-
-});
 
 
 
---
+++ src/main/resources/static/js/controller/cliente-controller.js
@@ -0,0 +1,54 @@
+appCliente.controller("indexController", function($scope, $http) {
+
+    $scope.clientes = [];
+    $scope.cliente={}; //Binding com o form
+
+    carregarClientes = function() {
+        $http({method:'GET', url:'http://localhost:8081/clientes'})
+            .then(function(response){
+                $scope.clientes = response.data;
+
+            }, function(response){
+                console.log(response.data);
+                console.log(response.status);
+        });
+    };
+
+    $scope.salvarCliente = function() {
+        $http({method:'POST', url:'http://localhost:8081/clientes', data:$scope.cliente})
+            .then(function(response){
+                //$scope.clientes.push(response.data);
+                carregarClientes();
+                $scope.cancelarAlteracaoCliente();
+
+            }, function(response){
+                console.log(response.data);
+                console.log(response.status);
+        });
+    };
+
+    $scope.excluirCliente = function(cliente) {
+        $http({method:'DELETE', url:'http://localhost:8081/clientes/'+cliente.id})
+            .then(function(response){
+            //Buscar posição  e remover cliente do Array.
+            pos = $scope.clientes.indexOf(cliente);
+            $scope.clientes.splice(pos, 1);
+
+            }, function(response){
+                console.log(response.data);
+                console.log(response.status);
+        });
+    };
+
+    $scope.alterarCliente = function(cli) {
+        $scope.cliente = angular.copy(cli);
+    };
+
+    $scope.cancelarAlteracaoCliente = function() {
+        $scope.cliente={};
+    };
+
+    carregarClientes();
+
+
+});
